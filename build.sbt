27dc4b5fcdacc27f851e7f6100f5076324ad45e9
Felipe <escalhao3@gmail.com>
2021-03-15 23:49:13 -0300

    apache-spark-3-course-init

---
+++ build.sbt
@@ -0,0 +1,18 @@
+name := "apache-spark-3-course"
+organization := "com.github.fescalhao"
+version := "0.1"
+scalaVersion := "2.12.10"
+autoScalaLibrary := false
+
+val sparkVersion = "3.1.1"
+
+val sparkDependencies = Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion
+)
+
+val testDependencies = Seq(
+  "org.scalatest" %% "scalatest" % "3.0.8" % Test
+)
+
+libraryDependencies ++= sparkDependencies ++ testDependencies
---
+++ src/main/scala/com/github/fescalhao/spark/example1/HelloSpark.scala
@@ -0,0 +1,63 @@
+package com.github.fescalhao.spark.example1
+
+import org.apache.log4j.Logger
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import java.util.Properties
+import scala.io.Source
+
+object HelloSpark extends Serializable {
+
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
+
+  def main(args: Array[String]): Unit = {
+
+    if (args.isEmpty) {
+      logger.error("Usage: HelloSpark filename")
+      System.exit(1)
+    }
+
+    logger.info("Starting Hello Spark")
+    val spark = SparkSession.builder()
+      .config(getSparkConf)
+      .getOrCreate()
+
+    // Process data
+    val sampleDF = loadSampleDF(spark, args(0))
+    val partitionedSampleDF = sampleDF.repartition(2)
+
+    val countDF = countByCountry(partitionedSampleDF)
+
+    logger.info(countDF.collect().mkString("->"))
+
+    logger.info("Finished Hello Spark")
+    scala.io.StdIn.readLine()
+    spark.stop()
+  }
+
+  def getSparkConf: SparkConf = {
+    val sparkConf = new SparkConf()
+    val props = new Properties()
+    props.load(Source.fromFile("spark.conf").bufferedReader())
+    props.forEach((x, y) =>
+      sparkConf.set(x.toString, y.toString)
+    )
+
+    sparkConf
+  }
+
+  def loadSampleDF(spark: SparkSession, dataFile: String): DataFrame = {
+    spark.read
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv(dataFile)
+  }
+
+  def countByCountry(sampleDF: DataFrame): DataFrame = {
+    sampleDF.where("Age < 40")
+      .select("Age", "Gender", "Country", "state")
+      .groupBy("Country")
+      .count()
+  }
+}
